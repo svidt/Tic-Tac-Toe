a370018a394829993fc3c2364f259674c349a97b
Emil <79602784+svidt@users.noreply.github.com>
2023-04-13 17:05:38 +0200

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by Kristian Emil Hansen Svidt on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
