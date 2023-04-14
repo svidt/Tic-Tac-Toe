80e06e405ae555b55a8dbcddfb2c3f791e9b6435
Emil <79602784+svidt@users.noreply.github.com>
2023-04-14 14:14:24 +0200

    commit

--- Tic-Tac-Toe/Models/GameModels.swift
+++ Tic-Tac-Toe/Models/GameModels.swift
@@ -5,7 +5,7 @@
 //  Created by Kristian Emil Hansen Svidt on 13/04/2023.
 //
 
-import Foundation
+import SwiftUI
 
 enum GameType {
     case single, bot, peer, undetermined
@@ -22,4 +22,41 @@ enum GameType {
             return ""
         }
     }
+}
+
+enum GamePiece: String {
+case x, o
+    var image: Image {
+        Image(self.rawValue)
+    }
+}
+
+struct Player {
+    let gamePiece: GamePiece
+    var name: String
+    var moves: [Int] = []
+    var isCurrent = false
+    var isWinner: Bool {
+        for moves in Move.winningMoves {
+            if moves.allSatisfy(self.moves.contains) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+enum Move {
+    static var all = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    
+    static var winningMoves = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7],
+    ]
 }
